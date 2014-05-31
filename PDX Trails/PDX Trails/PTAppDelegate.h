9343e2953f22800c2478af1a5e52d52620b7004f
Marc Charbonneau <marc@mbcharbonneau.com>
2014-05-31 12:44:19 -0700

    Create Xcode project.

---
+++ PDX Trails/PDX Trails/PTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PTAppDelegate.h
+//  PDX Trails
+//
+//  Created by Marc Charbonneau on 5/31/14.
+//  Copyright (c) 2014 Code for Portland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PDX Trails/PDX Trails/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PDX Trails
+//
+//  Created by Marc Charbonneau on 5/31/14.
+//  Copyright (c) 2014 Code for Portland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PTAppDelegate class]));
+    }
+}
